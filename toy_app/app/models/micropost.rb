0ad794e5c8de3a84098ee032f5a02b7b6241f64b
hunger <hunger@jirengu.com>
2016-10-26 01:39:37 +0800

    2.7 存在性验证

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :content, length: { maximum: 20, minimum: 6 }
+  validates :content, length: { maximum: 20, minimum: 6 }, presence: true
+
 end
